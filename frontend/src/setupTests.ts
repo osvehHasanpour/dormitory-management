2a2c4e61f5fe2757b92761a695c4c1b1a869a7ed
Mahshid <Mahshidmm8484@gmail.com>
2026-07-07 15:09:26 +0330

    Update setupTests.ts

--- frontend/src/setupTests.ts
+++ frontend/src/setupTests.ts
@@ -3,9 +3,9 @@ import "@testing-library/jest-dom";
 import { TextDecoder, TextEncoder } from "node:util";
 
 if (!globalThis.TextEncoder) {
-  globalThis.TextEncoder = TextEncoder;
+  globalThis.TextEncoder = TextEncoder as typeof globalThis.TextEncoder;
 }
 
 if (!globalThis.TextDecoder) {
-  globalThis.TextDecoder = TextDecoder;
+  globalThis.TextDecoder = TextDecoder as typeof globalThis.TextDecoder;
 }
