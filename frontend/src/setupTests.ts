1c007ca336f72bf3ec98b2e9a351db1db0a999e3
Osveh Hasanpour <hasanpourosveh@gmail.com>
2026-07-07 14:50:11 +0330

    fixing docker

--- frontend/src/setupTests.ts
+++ frontend/src/setupTests.ts
@@ -3,11 +3,9 @@ import "@testing-library/jest-dom";
 import { TextDecoder, TextEncoder } from "node:util";
 
 if (!globalThis.TextEncoder) {
-  // @ts-expect-error - polyfill for jsdom environment
   globalThis.TextEncoder = TextEncoder;
 }
 
 if (!globalThis.TextDecoder) {
-  // @ts-expect-error - polyfill for jsdom environment
   globalThis.TextDecoder = TextDecoder;
 }
